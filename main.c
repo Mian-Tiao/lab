50252e9f9da41f384bc2a5a2f88f36b1f452771f
Cookieorange735 <hbpschc0000@gmail.com>
2025-12-05 10:38:19 +0800

    chore: add automated test script

--- main.c
+++ main.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
 
+void Calculator (int minuend, int subtrahend){
+    int difference = minuend - subtrahend;
+    printf("The number is %d", difference);
+}
+
 int main() {
     printf("=== C Language Calculator ===\n");
+
+    int number_1 = 14;
+    int number_2 = 735;
+
+    Calculator(number_1, number_2);
+
     // Operations placeholders
     printf("System Check OK.\n");
     return 0;
