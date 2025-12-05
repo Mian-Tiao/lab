3cbd7bee70c4ab0d082a57ec44ec4982887b6433
unknown <eric1060156@gmail.com>
2025-12-05 11:19:11 +0800

    fix: resolve test_runner.bat merge conflict

--- test_runner.sh
+++ test_runner.sh
@@ -1,18 +1,3 @@
-<<<<<<< HEAD
-# !/ bin/ bash
-echo " Running Build Test..."
-gcc main.c -o app
-if [ $? -eq 0 ]; then
-echo " Compilation Successful!"
-./ app
-echo " Execution Test Passed!"
-exit 0
-else
-echo " Compilation Failed!"
-exit 1
-fi
-=======
-
 # !/ bin/ bash
     echo " Running Build Test..."
     gcc main.c -o app
@@ -25,5 +10,3 @@ else
     echo " Compilation Failed!"
     exit 1
 fi
-
->>>>>>> 2b20e4947ad427379763aebfc1070a5cdb041cf0
